9ba6002b1bd498641540f8092327afb2cdd6d71d
Khoa Tran <khoabin-star@huskers.unl.edu>
2020-08-19 00:10:53 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Khoa Tran
+ *         ktran18@cse.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Khoa Tran\n");
+  printf("Computer Science and Mathematics\n");
+
+  return 0;
+}
